ffe2fa6404cf139369bfbb1a90cc55905f668ebb
Markozoalor <MARKMARCANE@GMAIL.COM>
2016-09-17 16:24:22 +0300

    I added a slide-delete feature to the app

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -6,5 +6,36 @@ import {NavController} from 'ionic-angular';
 })
 export class AboutPage {
   constructor(private navCtrl: NavController) {
-  }
+                     this.items = [
+        {title: 'item1'},
+        {title: 'item2'},
+        {title: 'item3'},
+        {title: 'item4'},
+        {title: 'item5'},
+        {title: 'item6'}
+    ];
+
 }
+
+removeItem(item){
+ 
+    for(i = 0; i < this.items.length; i++) {
+ 
+      if(this.items[i] == item){
+        this.items.splice(i, 1);
+      }
+ 
+    }
+ 
+  }
+ 
+}
+
+
+
+
+
+
+
+
+  
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,35 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+declare var google;
+ 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController) {
+ 
+  }
+ 
+  ionViewLoaded(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
   }
 }
